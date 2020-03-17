f55becdccdfd57fe1f25141459c9efd6f31ef48f
Vladislav Mosin <vmos1999@mail.ru>
2020-03-17 21:27:38 +0300

    Test initialized

---
+++ src/test/java/inc/roguelike/babusya/map/RectangularMapTest.kt
@@ -0,0 +1,10 @@
+package inc.roguelike.babusya.map
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class RectangularMapTest {
+
+    @org.junit.jupiter.api.Test
+    fun serialize() {
+    }
+}
