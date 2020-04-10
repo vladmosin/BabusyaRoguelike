4e545d8bca3dd787405b648157bab7f8c251ce03
Makhnev Konstantin <mah.kons@gmail.com>
2020-04-10 19:17:07 +0300

    add TODO ConfusedDecorator

---
+++ src/main/kotlin/inc/roguelike/babusya/controllers/ConfusedDecorator.kt
@@ -0,0 +1,14 @@
+package inc.roguelike.babusya.controllers
+
+import inc.roguelike.babusya.GameLog
+import inc.roguelike.babusya.gameElement.Creature
+
+class ConfusedDecorator : ActionController {
+    override fun makeTurn(creature: Creature) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun useLog(gameLog: GameLog) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
