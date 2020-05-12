2dc9d66a727233700a6c002988fc2ab8a671838c
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2020-05-12 21:59:07 +0300

    Update proto

--- src/main/java/inc/roguelike/babusya/network/gen/GameGrpc.java
+++ src/main/java/inc/roguelike/babusya/network/gen/GameGrpc.java
@@ -1,4 +1,4 @@
-package inc.roguelike.babusya.network;
+package inc.roguelike.babusya.network.gen;
 
 import static io.grpc.MethodDescriptor.generateFullMethodName;
 import static io.grpc.stub.ClientCalls.asyncBidiStreamingCall;
@@ -19,37 +19,37 @@ import static io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall;
  */
 @javax.annotation.Generated(
     value = "by gRPC proto compiler (version 1.28.1)",
-    comments = "Source: service.proto")
+    comments = "Source: game.proto")
 public final class GameGrpc {
 
   private GameGrpc() {}
 
-  public static final String SERVICE_NAME = "inc.roguelike.babusya.network.Game";
+  public static final String SERVICE_NAME = "inc.roguelike.babusya.network.gen.Game";
 
   // Static method descriptors that strictly reflect the proto.
-  private static volatile io.grpc.MethodDescriptor<inc.roguelike.babusya.network.Empty,
-      inc.roguelike.babusya.network.Message> getReceiveMessageMethod;
+  private static volatile io.grpc.MethodDescriptor<inc.roguelike.babusya.network.gen.Empty,
+      inc.roguelike.babusya.network.gen.Message> getReceiveMessageMethod;
 
   @io.grpc.stub.annotations.RpcMethod(
       fullMethodName = SERVICE_NAME + '/' + "receiveMessage",
-      requestType = inc.roguelike.babusya.network.Empty.class,
-      responseType = inc.roguelike.babusya.network.Message.class,
+      requestType = inc.roguelike.babusya.network.gen.Empty.class,
+      responseType = inc.roguelike.babusya.network.gen.Message.class,
       methodType = io.grpc.MethodDescriptor.MethodType.SERVER_STREAMING)
-  public static io.grpc.MethodDescriptor<inc.roguelike.babusya.network.Empty,
-      inc.roguelike.babusya.network.Message> getReceiveMessageMethod() {
-    io.grpc.MethodDescriptor<inc.roguelike.babusya.network.Empty, inc.roguelike.babusya.network.Message> getReceiveMessageMethod;
+  public static io.grpc.MethodDescriptor<inc.roguelike.babusya.network.gen.Empty,
+      inc.roguelike.babusya.network.gen.Message> getReceiveMessageMethod() {
+    io.grpc.MethodDescriptor<inc.roguelike.babusya.network.gen.Empty, inc.roguelike.babusya.network.gen.Message> getReceiveMessageMethod;
     if ((getReceiveMessageMethod = GameGrpc.getReceiveMessageMethod) == null) {
       synchronized (GameGrpc.class) {
         if ((getReceiveMessageMethod = GameGrpc.getReceiveMessageMethod) == null) {
           GameGrpc.getReceiveMessageMethod = getReceiveMessageMethod =
-              io.grpc.MethodDescriptor.<inc.roguelike.babusya.network.Empty, inc.roguelike.babusya.network.Message>newBuilder()
+              io.grpc.MethodDescriptor.<inc.roguelike.babusya.network.gen.Empty, inc.roguelike.babusya.network.gen.Message>newBuilder()
               .setType(io.grpc.MethodDescriptor.MethodType.SERVER_STREAMING)
               .setFullMethodName(generateFullMethodName(SERVICE_NAME, "receiveMessage"))
               .setSampledToLocalTracing(true)
               .setRequestMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
-                  inc.roguelike.babusya.network.Empty.getDefaultInstance()))
+                  inc.roguelike.babusya.network.gen.Empty.getDefaultInstance()))
               .setResponseMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
-                  inc.roguelike.babusya.network.Message.getDefaultInstance()))
+                  inc.roguelike.babusya.network.gen.Message.getDefaultInstance()))
               .setSchemaDescriptor(new GameMethodDescriptorSupplier("receiveMessage"))
               .build();
         }
@@ -58,29 +58,29 @@ public final class GameGrpc {
     return getReceiveMessageMethod;
   }
 
-  private static volatile io.grpc.MethodDescriptor<inc.roguelike.babusya.network.Message,
-      inc.roguelike.babusya.network.Empty> getSendMessageMethod;
+  private static volatile io.grpc.MethodDescriptor<inc.roguelike.babusya.network.gen.Message,
+      inc.roguelike.babusya.network.gen.Empty> getSendMessageMethod;
 
   @io.grpc.stub.annotations.RpcMethod(
       fullMethodName = SERVICE_NAME + '/' + "sendMessage",
-      requestType = inc.roguelike.babusya.network.Message.class,
-      responseType = inc.roguelike.babusya.network.Empty.class,
+      requestType = inc.roguelike.babusya.network.gen.Message.class,
+      responseType = inc.roguelike.babusya.network.gen.Empty.class,
       methodType = io.grpc.MethodDescriptor.MethodType.UNARY)
-  public static io.grpc.MethodDescriptor<inc.roguelike.babusya.network.Message,
-      inc.roguelike.babusya.network.Empty> getSendMessageMethod() {
-    io.grpc.MethodDescriptor<inc.roguelike.babusya.network.Message, inc.roguelike.babusya.network.Empty> getSendMessageMethod;
+  public static io.grpc.MethodDescriptor<inc.roguelike.babusya.network.gen.Message,
+      inc.roguelike.babusya.network.gen.Empty> getSendMessageMethod() {
+    io.grpc.MethodDescriptor<inc.roguelike.babusya.network.gen.Message, inc.roguelike.babusya.network.gen.Empty> getSendMessageMethod;
     if ((getSendMessageMethod = GameGrpc.getSendMessageMethod) == null) {
       synchronized (GameGrpc.class) {
         if ((getSendMessageMethod = GameGrpc.getSendMessageMethod) == null) {
           GameGrpc.getSendMessageMethod = getSendMessageMethod =
-              io.grpc.MethodDescriptor.<inc.roguelike.babusya.network.Message, inc.roguelike.babusya.network.Empty>newBuilder()
+              io.grpc.MethodDescriptor.<inc.roguelike.babusya.network.gen.Message, inc.roguelike.babusya.network.gen.Empty>newBuilder()
               .setType(io.grpc.MethodDescriptor.MethodType.UNARY)
               .setFullMethodName(generateFullMethodName(SERVICE_NAME, "sendMessage"))
               .setSampledToLocalTracing(true)
               .setRequestMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
-                  inc.roguelike.babusya.network.Message.getDefaultInstance()))
+                  inc.roguelike.babusya.network.gen.Message.getDefaultInstance()))
               .setResponseMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
-                  inc.roguelike.babusya.network.Empty.getDefaultInstance()))
+                  inc.roguelike.babusya.network.gen.Empty.getDefaultInstance()))
               .setSchemaDescriptor(new GameMethodDescriptorSupplier("sendMessage"))
               .build();
         }
@@ -139,15 +139,15 @@ public final class GameGrpc {
 
     /**
      */
-    public void receiveMessage(inc.roguelike.babusya.network.Empty request,
-        io.grpc.stub.StreamObserver<inc.roguelike.babusya.network.Message> responseObserver) {
+    public void receiveMessage(inc.roguelike.babusya.network.gen.Empty request,
+        io.grpc.stub.StreamObserver<inc.roguelike.babusya.network.gen.Message> responseObserver) {
       asyncUnimplementedUnaryCall(getReceiveMessageMethod(), responseObserver);
     }
 
     /**
      */
-    public void sendMessage(inc.roguelike.babusya.network.Message request,
-        io.grpc.stub.StreamObserver<inc.roguelike.babusya.network.Empty> responseObserver) {
+    public void sendMessage(inc.roguelike.babusya.network.gen.Message request,
+        io.grpc.stub.StreamObserver<inc.roguelike.babusya.network.gen.Empty> responseObserver) {
       asyncUnimplementedUnaryCall(getSendMessageMethod(), responseObserver);
     }
 
@@ -157,15 +157,15 @@ public final class GameGrpc {
             getReceiveMessageMethod(),
             asyncServerStreamingCall(
               new MethodHandlers<
-                inc.roguelike.babusya.network.Empty,
-                inc.roguelike.babusya.network.Message>(
+                inc.roguelike.babusya.network.gen.Empty,
+                inc.roguelike.babusya.network.gen.Message>(
                   this, METHODID_RECEIVE_MESSAGE)))
           .addMethod(
             getSendMessageMethod(),
             asyncUnaryCall(
               new MethodHandlers<
-                inc.roguelike.babusya.network.Message,
-                inc.roguelike.babusya.network.Empty>(
+                inc.roguelike.babusya.network.gen.Message,
+                inc.roguelike.babusya.network.gen.Empty>(
                   this, METHODID_SEND_MESSAGE)))
           .build();
     }
@@ -187,16 +187,16 @@ public final class GameGrpc {
 
     /**
      */
-    public void receiveMessage(inc.roguelike.babusya.network.Empty request,
-        io.grpc.stub.StreamObserver<inc.roguelike.babusya.network.Message> responseObserver) {
+    public void receiveMessage(inc.roguelike.babusya.network.gen.Empty request,
+        io.grpc.stub.StreamObserver<inc.roguelike.babusya.network.gen.Message> responseObserver) {
       asyncServerStreamingCall(
           getChannel().newCall(getReceiveMessageMethod(), getCallOptions()), request, responseObserver);
     }
 
     /**
      */
-    public void sendMessage(inc.roguelike.babusya.network.Message request,
-        io.grpc.stub.StreamObserver<inc.roguelike.babusya.network.Empty> responseObserver) {
+    public void sendMessage(inc.roguelike.babusya.network.gen.Message request,
+        io.grpc.stub.StreamObserver<inc.roguelike.babusya.network.gen.Empty> responseObserver) {
       asyncUnaryCall(
           getChannel().newCall(getSendMessageMethod(), getCallOptions()), request, responseObserver);
     }
@@ -218,15 +218,15 @@ public final class GameGrpc {
 
     /**
      */
-    public java.util.Iterator<inc.roguelike.babusya.network.Message> receiveMessage(
-        inc.roguelike.babusya.network.Empty request) {
+    public java.util.Iterator<inc.roguelike.babusya.network.gen.Message> receiveMessage(
+        inc.roguelike.babusya.network.gen.Empty request) {
       return blockingServerStreamingCall(
           getChannel(), getReceiveMessageMethod(), getCallOptions(), request);
     }
 
     /**
      */
-    public inc.roguelike.babusya.network.Empty sendMessage(inc.roguelike.babusya.network.Message request) {
+    public inc.roguelike.babusya.network.gen.Empty sendMessage(inc.roguelike.babusya.network.gen.Message request) {
       return blockingUnaryCall(
           getChannel(), getSendMessageMethod(), getCallOptions(), request);
     }
@@ -248,8 +248,8 @@ public final class GameGrpc {
 
     /**
      */
-    public com.google.common.util.concurrent.ListenableFuture<inc.roguelike.babusya.network.Empty> sendMessage(
-        inc.roguelike.babusya.network.Message request) {
+    public com.google.common.util.concurrent.ListenableFuture<inc.roguelike.babusya.network.gen.Empty> sendMessage(
+        inc.roguelike.babusya.network.gen.Message request) {
       return futureUnaryCall(
           getChannel().newCall(getSendMessageMethod(), getCallOptions()), request);
     }
@@ -276,12 +276,12 @@ public final class GameGrpc {
     public void invoke(Req request, io.grpc.stub.StreamObserver<Resp> responseObserver) {
       switch (methodId) {
         case METHODID_RECEIVE_MESSAGE:
-          serviceImpl.receiveMessage((inc.roguelike.babusya.network.Empty) request,
-              (io.grpc.stub.StreamObserver<inc.roguelike.babusya.network.Message>) responseObserver);
+          serviceImpl.receiveMessage((inc.roguelike.babusya.network.gen.Empty) request,
+              (io.grpc.stub.StreamObserver<inc.roguelike.babusya.network.gen.Message>) responseObserver);
           break;
         case METHODID_SEND_MESSAGE:
-          serviceImpl.sendMessage((inc.roguelike.babusya.network.Message) request,
-              (io.grpc.stub.StreamObserver<inc.roguelike.babusya.network.Empty>) responseObserver);
+          serviceImpl.sendMessage((inc.roguelike.babusya.network.gen.Message) request,
+              (io.grpc.stub.StreamObserver<inc.roguelike.babusya.network.gen.Empty>) responseObserver);
           break;
         default:
           throw new AssertionError();
@@ -305,7 +305,7 @@ public final class GameGrpc {
 
     @java.lang.Override
     public com.google.protobuf.Descriptors.FileDescriptor getFileDescriptor() {
-      return inc.roguelike.babusya.network.Service.getDescriptor();
+      return inc.roguelike.babusya.network.gen.GameOuterClass.getDescriptor();
     }
 
     @java.lang.Override
