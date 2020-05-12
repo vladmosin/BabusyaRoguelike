5b950c6b0224b8a9b67fad27210ce23e7e734cc7
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2020-05-13 01:01:18 +0300

    Implement client

--- src/main/kotlin/inc/roguelike/babusya/network/Client.kt
+++ src/main/kotlin/inc/roguelike/babusya/network/Client.kt
@@ -1,59 +1,63 @@
 package inc.roguelike.babusya.network
 
-import inc.roguelike.babusya.inputListeners.InputData
 import inc.roguelike.babusya.network.gen.*
 import inc.roguelike.babusya.network.gen.Player
-
-import io.grpc.ManagedChannel
 import io.grpc.ManagedChannelBuilder
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.asExecutor
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.runBlocking
 import java.io.Closeable
 import java.util.concurrent.TimeUnit
 
+
 /**
  * Sends and receives messages
  * */
-class Client(address: String, port: Int) : Closeable {
+class Client(address: String, port: Int, val login: String, val id: Int) : Closeable {
 
     private val SHUTDOWN_TIMEOUT: Long = 5
 
     val channel = ManagedChannelBuilder.forAddress(address, port).usePlaintext().build()
     val stub = GameGrpcKt.GameCoroutineStub(channel)
 
+    fun createRoom(roomId: Int): Boolean = runBlocking {
+        val room = Room.newBuilder().setId(roomId).build()
+        val player = Player
+            .newBuilder()
+            .setId(id)
+            .setLogin(login)
+            .setRoom(room)
+            .build()
+        val response = stub.createRoom(player)
+        return@runBlocking response.status
+    }
+
+    fun getRooms(): List<Room>? = runBlocking {
+        return@runBlocking stub.getRooms(Empty.getDefaultInstance()).toList()
+    }
+
+    fun joinRoom(roomId: Int): Boolean = runBlocking {
+        val room = Room.newBuilder().setId(roomId).build()
+        val player = Player
+            .newBuilder()
+            .setId(id)
+            .setLogin(login)
+            .setRoom(room)
+            .build()
+        val response = stub.joinRoom(player)
+        return@runBlocking response.status
+    }
+
     fun getState(): Message = runBlocking {
         val state: State = stub.getState(Empty.getDefaultInstance())
         return@runBlocking Message(state.level, state.ends, state.log)
     }
 
-//    fun createRoom(roomId: Int): Boolean = runBlocking {
-//        val room = Room.newBuilder().setId(roomId).build()
-//        val player = inc.roguelike.babusya.network.gen.Player
-//            .newBuilder()
-//            .setId()
-//            .
-//        return@runBlocking true
-//    }
-
-    fun receiveMessage(): Message {
-        TODO()
-    }
-
-    fun sendMessage(message: Message) {
-//        val request = inc.roguelike.babusya.network.gen.Message.newBuilder()
-//            .setLevel(message.serializedLevel)
-//            .setEnds(message.gameEnds)
-//            .setLog(message.serializedGameLog)
-//            .build()
-    }
-
-    fun sendPlayerInput(inputData: InputData) {
-        TODO()
-    }
-
-    fun receiveInputData(): InputData {
-        TODO()
+    fun sendInputData(data: String) = runBlocking {
+        val inputData = inc.roguelike.babusya.network.gen.InputData.newBuilder()
+            .setData(data)
+            .setPlayerId(id)
+            .build()
+        stub.sendInputData(inputData)
     }
 
     override fun close() {
