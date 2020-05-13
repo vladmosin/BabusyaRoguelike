3eae8d5004d69e02eeb6dfe17ae829a03f5e0f73
Makhnev Konstantin <mah.kons@gmail.com>
2020-05-13 20:45:09 +0300

    remove mutex

--- src/main/kotlin/inc/roguelike/babusya/network/Player.kt
+++ src/main/kotlin/inc/roguelike/babusya/network/Player.kt
@@ -2,30 +2,20 @@ package inc.roguelike.babusya.network
 
 import inc.roguelike.babusya.element.concrete.Hero
 import inc.roguelike.babusya.inputListeners.InputData
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.sync.Mutex
 
 class Player(var hero: Hero?, val id: Int) {
-    var lastInputData: InputData? = null
-    val mutex = Mutex(true)
+    private val inputDataChannel = Channel<InputData>(capacity = Channel.CONFLATED)
 
     suspend fun receiveInputData(): InputData {
         println("RECEIVE INPUT DATA BEGIN")
         while (true) {
-//            println("WTF $lastInputData")
-            if (lastInputData == null) {
-                mutex.lock()
-            }
-            if (lastInputData != null) {
-                val result = lastInputData
-                lastInputData = null
-                println("RECEIVE INPUT DATA END result=$result")
-                return result!!
-            }
+            return inputDataChannel.receive()
         }
     }
 
-    fun setInputData(inputData: InputData) {
-        lastInputData = inputData
-        mutex.unlock()
+    suspend fun setInputData(inputData: InputData) {
+        inputDataChannel.send(inputData)
     }
 }
