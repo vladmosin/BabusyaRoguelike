68b06a635463e5a95b6d2acc9ef751551f2790a0
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2020-05-13 22:42:42 +0300

    Add timeout

--- src/main/kotlin/inc/roguelike/babusya/network/Player.kt
+++ src/main/kotlin/inc/roguelike/babusya/network/Player.kt
@@ -3,15 +3,19 @@ package inc.roguelike.babusya.network
 import inc.roguelike.babusya.element.concrete.Hero
 import inc.roguelike.babusya.inputListeners.InputData
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.withTimeout
 
 class Player(var hero: Hero?, val id: Int) {
     private val inputDataChannel = Channel<InputData>(capacity = Channel.CONFLATED)
 
     suspend fun receiveInputData(): InputData {
         println("RECEIVE INPUT DATA BEGIN")
-        while (true) {
-            return inputDataChannel.receive()
+        try {
+            withTimeout(timeMillis = 10000) {
+                return@withTimeout inputDataChannel.receive()
+            }
+        } finally {
+            TODO()
         }
     }
 
